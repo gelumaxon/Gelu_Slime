daf27e4a53714b0fa7ffcc6d6669ed3767a69f1a
gelumaxon <67863944+gelumaxon@users.noreply.github.com>
2022-06-17 21:07:33 -0300

    Create main.php

---
+++ src/Gelu/main.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Gelu;
+
+use pocketmine\Server;
+use pocketmine\player\Player;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+
+use pocketmine\world\particle\FlameParticle;
+
+use pocketmine\math\Vector3;
+
+
+class main extends PluginBase implements Listener{
+  public function onEnable(): void {
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+   public $noDamage = [];
+  
+   public function onMove(PlayerMoveEvent $event){
+    $player = $event->getPlayer();
+    $world = $player->getWorld();
+    $x = $player->getPosition()->getX();
+    $y = $player->getPosition()->getY();
+    $z = $player->getPosition()->getZ();
+    $block = $player->getWorld()->getBlock(new Vector3($x, $y - 0.5, $z));
+    if($block->getId() === 165){
+        $this->noDamage[$player->getName()] = $player->getName();
+         $player->setMotion(new Vector3(0, 0.8, 0));
+         $world->addParticle(new Vector3($x + 0.5, $y, $z), new FlameParticle($x + 0.5, $y, $z));
+         $world->addParticle(new Vector3($x, $y, $z + 0.5), new FlameParticle($x, $y, $z + 0.5));
+         $world->addParticle(new Vector3($x - 0.5, $y, $z), new FlameParticle($x - 0.5, $y, $z));
+         $world->addParticle(new Vector3($x, $y, $z - 0.5), new FlameParticle($x, $y, $z - 0.5));
+    }
+   }
+       public function onDamage(EntityDamageEvent $event){
+      if($event->getEntity() instanceof Player){
+  if($event->getCause() == EntityDamageEvent::CAUSE_FALL){
+    if(isset($this->noDamage[$event->getEntity()->getName()])){
+     $event->cancel();
+          unset($this->noDamage[$event->getEntity()->getName()]);
+  }
+ }
+     }
+   }
+}
